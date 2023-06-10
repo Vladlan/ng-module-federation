834e1e68e30447a3baf273dd73c7ad5017bd87b9
Vladlan <lanvlad911@gmail.com>
2023-06-10 20:29:00 +0200

    Move info about mfe1 to manifest

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,14 +1,20 @@
-// import('./bootstrap')
+// import('./bootstrap')                                                      #1
 // 	.catch(err => console.error(err));
 
-import { loadRemoteEntry } from '@angular-architects/module-federation';
+// import { loadRemoteEntry } from '@angular-architects/module-federation';   #2
+import { loadManifest } from '@angular-architects/module-federation';
 
-Promise.all([
-  loadRemoteEntry({
-    type: 'module',
-    remoteEntry: 'http://localhost:4201/remoteEntry.js',
-  }),
-])
+loadManifest('assets/mf.manifest.json')
   .catch((err) => console.error('Error loading remote entries', err))
   .then(() => import('./bootstrap'))
   .catch((err) => console.error(err));
+
+// Promise.all([                                                              #2
+//   loadRemoteEntry({
+//     type: 'module',
+//     remoteEntry: 'http://localhost:4201/remoteEntry.js',
+//   }),
+// ])
+//   .catch((err) => console.error('Error loading remote entries', err))
+//   .then(() => import('./bootstrap'))
+//   .catch((err) => console.error(err));
